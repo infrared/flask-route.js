96ebedc299d2969178dedd592dd08b1e2fd56a1d
Michael Kroher <infrared@users.noreply.github.com>
2018-03-01 10:18:19 -0500

    added flask-route.js

---
+++ flask-route.js
@@ -0,0 +1,61 @@
+var FlaskRoute = function(routes) {
+  if (Object.prototype.toString.call(routes) != "[object Object]") {
+    alert('routes must be a dictionary');
+  }
+  this.routes = routes;
+}
+
+FlaskRoute.prototype.get_route = function(endpoint, args) {
+  var self = this;
+
+  if (!self.routes.hasOwnProperty(endpoint)) {
+    alert('Could not find route endpoint: ' + endpoint);
+    return false;
+  }
+  if (!args) {
+    /**
+     * No arguments were passed to construct a url, so return the first
+     * route that does not require arguments.
+     * /api/foo/bar
+     * get_route(foo)
+     */
+    for (var x in self.routes[endpoint]) {
+      var route_args = self.routes[endpoint][x].match(/<(?:\w+\:)?(\w+)>/gi);
+      if (!route_args) {
+        return self.routes[endpoint][x];
+      }
+    }
+    alert('Could not construct a route for ' + endpoint + ' when no args were passed');
+    return false;
+  }
+
+  if (Object.prototype.toString.call(args) != "[object Object]") {
+    alert('args passed to get_route() must be a dictionary');
+    return false;
+  }
+  if (Object.keys(args).length == 0) {
+    alert('empty arguments passed to get_route()')
+    return false;
+  }
+  for (var x in self.routes[endpoint]) {
+    var route_args = self.routes[endpoint][x].match(/<(?:\w+\:)?(\w+)>/gi);
+    if (route_args) {
+      if (route_args.length == Object.keys(args).length) {
+        var temp_route = self.routes[endpoint][x]
+        for (var i in route_args) {
+          var arg = route_args[i].match(/<(?:(\w+\:))?(\w+)>/)[2];
+          var replace = route_args[i].match(/<(?:(\w+\:))?(\w+)>/)[0];
+          if (!args.hasOwnProperty(arg)) {
+            alert(arg + ' wanted but not passed');
+            return false;
+          }
+          temp_route = temp_route.replace(route_args[i], args[arg]);
+        }
+
+        return temp_route;
+      }
+    }
+  }
+  /* Reached here, return first route in endpoint list */
+  alert('Route could not be constructed');
+}
